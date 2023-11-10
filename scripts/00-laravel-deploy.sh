f3adae9c334445fa1ee6110556852aea53aa9c28
TALABI AYOMIDE <flioolawale@gmail.com>
2023-11-10 16:31:37 +0100

    features(Live Server AutoDeploy Enhancement): Application Optimization Config

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -6,6 +6,9 @@ composer install --no-dev --working-dir=/var/www/html
 # echo "generating application key..."
 # php artisan key:generate --show
 
+echo "Optimization config..."
+php artisan optimize:clear
+
 echo "Caching config..."
 php artisan config:cache
 
