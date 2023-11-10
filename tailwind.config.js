9cb816b4c48244555e17d7c23f66fcba8f5eb00f
TALABI AYOMIDE <flioolawale@gmail.com>
2023-11-10 11:02:02 +0100

    features(addition): Initialized Tailwind and Webpack configuration

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./resources/**/*.blade.php"],
+  theme: {
+    screens: {
+      // xs: {
+      //   max: "394px",
+      // },
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      borderRadius: {
+        "3xs": "10px",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
